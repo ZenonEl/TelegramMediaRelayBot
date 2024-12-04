0a54cb0dba2aec11319ebfa6f1f3445098857006
ZenonEl <165126589+ZenonEl@users.noreply.github.com>
2024-12-04 15:28:26 +0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TikTokMediaRelayBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await TelegramBot.Start();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# TikTokMediaRelayBot
+ 
