e57f07f2a897422c62626471b529780042baccfa
ZenonEl <165126589+ZenonEl@users.noreply.github.com>
2025-01-01 19:14:29 +0400

    refactor(Database): migrate the functions of creating tables/DB

--- Database/Database.cs
+++ Database/Database.cs
@@ -1,75 +1,22 @@
 using MySql.Data.MySqlClient;
 using Serilog;
 using TelegramMediaRelayBot;
+using DataBase.DBCreating;
 
 namespace DataBase;
 
 public class CoreDB
 {
-    public static string connectionString = Config.sqlConnectionString;
+    public static string connectionString = Config.sqlConnectionString!;
 
     public static void initDB()
     {
-        CreateDatabase();
-        CreateUsersTable();
-        CreateContactsTable();
-        CreateMutedContactsTable();
-    }
-
-    private static void CreateDatabase()
-    {
-        string query = $"CREATE DATABASE IF NOT EXISTS {Config.databaseName};";
-        Utils.executeVoidQuery(query);
-    }
-
-    private static void CreateUsersTable()
-    {
-        string query = @$"
-            USE {Config.databaseName};
-            CREATE TABLE IF NOT EXISTS Users (
-                ID INT PRIMARY KEY AUTO_INCREMENT,
-                TelegramID BIGINT NOT NULL,
-                Name VARCHAR(255) NOT NULL,
-                Link VARCHAR(255) NOT NULL,
-                Status VARCHAR(255)
-            )";
-
-        Utils.executeVoidQuery(query);
-    }
-
-    private static void CreateContactsTable()
-    {
-        string query = @$"
-            USE {Config.databaseName};
-            CREATE TABLE IF NOT EXISTS Contacts (
-                UserId INT,
-                ContactId INT,
-                status VARCHAR(255),
-                PRIMARY KEY (UserId, ContactId),
-                FOREIGN KEY (UserId) REFERENCES Users(ID),
-                FOREIGN KEY (ContactId) REFERENCES Users(ID)
-            )";
-
-        Utils.executeVoidQuery(query);
-    }
-
-    private static void CreateMutedContactsTable()
-    {
-        string query = @$"
-            USE {Config.databaseName};
-            CREATE TABLE IF NOT EXISTS MutedContacts (
-                MutedId INT PRIMARY KEY AUTO_INCREMENT,
-                MutedByUserId INT NOT NULL,
-                MutedContactId INT NOT NULL,
-                MuteDate DATETIME NOT NULL,
-                ExpirationDate DATETIME NULL,
-                IsActive BOOLEAN NOT NULL DEFAULT TRUE,
-                UNIQUE (MutedByUserId, MutedContactId),
-                FOREIGN KEY (MutedByUserId) REFERENCES Users(ID),
-                FOREIGN KEY (MutedContactId) REFERENCES Users(ID)
-            )";
-
-        Utils.executeVoidQuery(query);
+        AllCreatingFunc.CreateDatabase();
+        AllCreatingFunc.CreateUsersTable();
+        AllCreatingFunc.CreateContactsTable();
+        AllCreatingFunc.CreateMutedContactsTable();
+        AllCreatingFunc.CreateGroupsTable();
+        AllCreatingFunc.CreateGroupMembersTable();
     }
 
     public static bool CheckExistsUser(long telegramID)
