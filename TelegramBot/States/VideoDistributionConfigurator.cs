48d8e434ea2b9fe613142f66a151c94e0b478e04
ZenonEl <165126589+ZenonEl@users.noreply.github.com>
2025-01-18 10:06:40 +0400

    feat(VideoDistributionConfigurator.cs): expand the functionality
    Added functionality for mailing to individual contacts/groups

--- TelegramBot/States/VideoDistributionConfigurator.cs
+++ TelegramBot/States/VideoDistributionConfigurator.cs
@@ -3,6 +3,7 @@ using Telegram.Bot.Types;
 using DataBase;
 using MediaTelegramBot.Utils;
 using TelegramMediaRelayBot;
+using Telegram.Bot.Types.Enums;
 
 
 namespace MediaTelegramBot;
@@ -15,6 +16,7 @@ public class ProcessVideoDC : IUserState
     public string text { get; set; }
     private string action = "";
     private List<long> targetUserIds = new List<long>();
+    private List<long> preparedTargetUserIds = new List<long>();
 
     public ProcessVideoDC(string Link, Message StatusMessage, string Text)
     {
@@ -46,26 +48,38 @@ public class ProcessVideoDC : IUserState
                             await PrepareTargetUserIds(chatId);
                             await botClient.EditMessageText(statusMessage.Chat.Id, statusMessage.MessageId, Config.GetResourceString("ConfirmDecision"), replyMarkup: KeyboardUtils.GetConfirmForActionKeyboardMarkup(), cancellationToken: cancellationToken);
                             break;
+
                         case "send_to_default_groups":
                             action = "send_to_default_groups";
                             await PrepareTargetUserIds(chatId);
                             await botClient.EditMessageText(statusMessage.Chat.Id, statusMessage.MessageId, Config.GetResourceString("ConfirmDecision"), replyMarkup: KeyboardUtils.GetConfirmForActionKeyboardMarkup(), cancellationToken: cancellationToken);
                             break;
+
                         case "send_to_specified_groups":
                             action = "send_to_specified_groups";
-                            await botClient.SendMessage(chatId, "Please enter group IDs separated by spaces:", cancellationToken: cancellationToken);
-                            currentState = UsersGroupState.ProcessData;
+                            List<string> groupInfos = UsersGroup.GetUserGroupInfoByUserId(DBforGetters.GetUserIDbyTelegramID(chatId));
+
+                            string messageText = groupInfos.Any() 
+                                ? $"{Config.GetResourceString("YourGroupsText")}\n{string.Join("\n", groupInfos)}" 
+                                : Config.GetResourceString("AltYourGroupsText");
+                            string text = $"{messageText}\n{Config.GetResourceString("PleaseEnterContactIDs")}";
+
+                            await botClient.EditMessageText(statusMessage.Chat.Id, statusMessage.MessageId, text, replyMarkup: KeyboardUtils.GetReturnButtonMarkup(), cancellationToken: cancellationToken, parseMode: ParseMode.Html);
+                            currentState = UsersStandartState.ProcessData;
                             break;
+
                         case "send_to_specified_users":
                             action = "send_to_specified_users";
-                            await botClient.SendMessage(chatId, "Please enter user IDs separated by spaces:", cancellationToken: cancellationToken);
-                            currentState = UsersGroupState.ProcessData;
+                            await botClient.EditMessageText(statusMessage.Chat.Id, statusMessage.MessageId, Config.GetResourceString("PleaseEnterContactIDs"), cancellationToken: cancellationToken);
+                            currentState = UsersStandartState.ProcessData;
                             break;
+
                         case "send_only_to_me":
                             action = "send_only_to_me";
                             await botClient.EditMessageText(statusMessage.Chat.Id, statusMessage.MessageId, Config.GetResourceString("ConfirmDecision"), replyMarkup: KeyboardUtils.GetConfirmForActionKeyboardMarkup(), cancellationToken: cancellationToken);
                             currentState = UsersStandartState.Finish;
                             break;
+
                         case "main_menu":
                             await Utils.Utils.HandleStateBreakCommand(botClient, update, chatId, removeReplyMarkup: false);
                             break;
@@ -73,8 +87,14 @@ public class ProcessVideoDC : IUserState
                 }
                 break;
 
-            case UsersGroupState.ProcessData:
-                if (update.Message != null)
+            case UsersStandartState.ProcessData:
+                if (update.CallbackQuery != null && update.CallbackQuery.Data == "main_menu")
+                {
+                    await botClient.EditMessageText(statusMessage.Chat.Id, statusMessage.MessageId, Config.GetResourceString("VideoDistributionQuestion"), replyMarkup: KeyboardUtils.GetVideoDistributionKeyboardMarkup(), cancellationToken: cancellationToken);
+                    currentState = UsersStandartState.ProcessAction;
+                    return;
+                }
+                else if (update.Message != null)
                 {
                     string input = update.Message.Text!;
                     if (input.Contains(" "))
@@ -82,24 +102,26 @@ public class ProcessVideoDC : IUserState
                         string[] ids = input.Split(' ');
                         if (ids.All(id => long.TryParse(id, out _)))
                         {
-                            targetUserIds = ids.Select(long.Parse).ToList();
+                            preparedTargetUserIds = ids.Select(long.Parse).ToList();
                             await PrepareTargetUserIds(chatId);
+                            await botClient.EditMessageText(statusMessage.Chat.Id, statusMessage.MessageId, Config.GetResourceString("ConfirmDecision"), replyMarkup: KeyboardUtils.GetConfirmForActionKeyboardMarkup(), cancellationToken: cancellationToken);
                         }
                         else
                         {
-                            await botClient.SendMessage(chatId, "Invalid input. Please enter numbers separated by spaces.", cancellationToken: cancellationToken);
+                            await botClient.SendMessage(chatId, Config.GetResourceString("InvalidInputNumbers"), cancellationToken: cancellationToken);
                         }
                     }
                     else
                     {
                         if (long.TryParse(input, out long id))
                         {
-                            targetUserIds.Add(id);
+                            preparedTargetUserIds.Add(id);
                             await PrepareTargetUserIds(chatId);
+                            await botClient.EditMessageText(statusMessage.Chat.Id, statusMessage.MessageId, Config.GetResourceString("ConfirmDecision"), replyMarkup: KeyboardUtils.GetConfirmForActionKeyboardMarkup(), cancellationToken: cancellationToken);
                         }
                         else
                         {
-                            await botClient.SendMessage(chatId, "Invalid input. Please enter a number.", cancellationToken: cancellationToken);
+                            await botClient.SendMessage(chatId, Config.GetResourceString("InvalidInputNumbers"), cancellationToken: cancellationToken);
                         }
                     }
                 }
@@ -113,6 +135,7 @@ public class ProcessVideoDC : IUserState
                     currentState = UsersStandartState.ProcessAction;
                     return;
                 }
+
                 await botClient.EditMessageText(statusMessage.Chat.Id, statusMessage.MessageId, Config.GetResourceString("WaitDownloadingVideo"), cancellationToken: cancellationToken);
                 TelegramBot.userStates.Remove(chatId);
                 _ = TelegramBot.HandleVideoRequest(botClient, link, chatId, statusMessage, targetUserIds, caption: text);
@@ -124,11 +147,12 @@ public class ProcessVideoDC : IUserState
     {
         int userId = DBforGetters.GetUserIDbyTelegramID(chatId);
         List<long> mutedByUserIds = DBforGetters.GetUsersIdForMuteContactId(userId);
+        List<long> contactUserTGIds = new List<long>();
 
         switch (action)
         {
             case "send_to_all_contacts":
-                List<long> contactUserTGIds = await CoreDB.GetAllContactUserTGIds(userId);
+                contactUserTGIds = await CoreDB.GetAllContactUserTGIds(userId);
                 targetUserIds = contactUserTGIds.Except(mutedByUserIds).ToList();
                 break;
 
@@ -140,8 +164,28 @@ public class ProcessVideoDC : IUserState
                     .Select(DBforGetters.GetTelegramIDbyUserID)
                     .ToList();
                 break;
+
             case "send_to_specified_groups":
+                List<int> contactUserIds = new List<int>();
+                foreach (int groupId in preparedTargetUserIds)
+                {
+                    contactUserIds.AddRange(DBforGroups.GetAllUsersInGroup(groupId, userId));
+                }
+
+                targetUserIds = contactUserIds
+                    .Where(contactId => !mutedByUserIds.Contains(DBforGetters.GetTelegramIDbyUserID(contactId)))
+                    .Select(DBforGetters.GetTelegramIDbyUserID)
+                    .ToList();
+                    break;
+
             case "send_to_specified_users":
+                foreach (int contactId in preparedTargetUserIds)
+                {
+                    contactUserTGIds.Add(DBforGetters.GetTelegramIDbyUserID(contactId));
+                }
+                List<long> allContactUserTGIds = await CoreDB.GetAllContactUserTGIds(userId);
+                List<long> filteredContactUserTGIds = contactUserTGIds.Except(allContactUserTGIds).ToList();
+                targetUserIds = contactUserTGIds.Except(mutedByUserIds).ToList();
                 break;
         }
 
