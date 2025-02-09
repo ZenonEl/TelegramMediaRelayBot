2fc7a6b847ab721ef19c1979fd2a4dfd761ea12a
ZenonEl <165126589+ZenonEl@users.noreply.github.com>
2025-02-09 08:17:34 +0400

    refactor(PrivateUpdateHandler.cs) extract code to function and file

--- TelegramBot/Handlers/PrivateUpdateHandler.cs
+++ TelegramBot/Handlers/PrivateUpdateHandler.cs
@@ -72,76 +72,8 @@ public class PrivateUpdateHandler
 
             if (defaultAction == UsersAction.OFF) return;
 
-            _ = Task.Run(async () =>
-            {
-                try
-                {
-                    await Task.Delay(TimeSpan.FromSeconds(defaultCondition), timeoutCTS.Token);
-
-                    List<long> targetUserIds = new List<long>();
-                    List<long> mutedByUserIds = new List<long>();
-                    List<int> userIds = new List<int>();
-
-                    int actionId = DBforDefaultActions.GetDefaultActionId(userId, UsersActionTypes.DEFAULT_MEDIA_DISTRIBUTION); 
-                    mutedByUserIds = DBforGetters.GetUsersIdForMuteContactId(userId);
-
-                    switch (defaultAction)
-                    {
-                        case UsersAction.SEND_MEDIA_TO_ALL_CONTACTS:
-                            List<long> contactUserTGIds = await CoreDB.GetAllContactUserTGIds(userId);
-                            targetUserIds = contactUserTGIds.Except(mutedByUserIds).ToList();
-                            break;
-
-                        case UsersAction.SEND_MEDIA_TO_DEFAULT_GROUPS:
-                            userIds = DBforGroups.GetAllUsersInDefaultEnabledGroups(userId);
-
-                            targetUserIds = userIds
-                                .Where(contactId => !mutedByUserIds.Contains(DBforGetters.GetTelegramIDbyUserID(contactId)))
-                                .Select(DBforGetters.GetTelegramIDbyUserID)
-                                .ToList();
-                            break;
-                        
-                        case UsersAction.SEND_MEDIA_TO_SPECIFIED_GROUPS:
-                            List<int> groupIds = DBforDefaultActions.GetAllDefaultUsersActionTargets(userId, TargetTypes.GROUP, actionId);
-                            
-                            foreach (int groupId in groupIds)
-                            {
-                                userIds.AddRange(DBforGroups.GetAllUsersIdsInGroup(groupId));
-                            }
-
-                            targetUserIds = userIds
-                                .Where(contactId => !mutedByUserIds.Contains(DBforGetters.GetTelegramIDbyUserID(contactId)))
-                                .Select(DBforGetters.GetTelegramIDbyUserID)
-                                .ToList();
-                            break;
-                        
-                        case UsersAction.SEND_MEDIA_TO_SPECIFIED_USERS:
-                            userIds = DBforDefaultActions.GetAllDefaultUsersActionTargets(userId, TargetTypes.USER, actionId);
-
-                            targetUserIds = userIds
-                                .Where(contactId => !mutedByUserIds.Contains(DBforGetters.GetTelegramIDbyUserID(contactId)))
-                                .Select(DBforGetters.GetTelegramIDbyUserID)
-                                .ToList();
-
-                            break;
-                    }
-
-                    if (TelegramBot.userStates.TryGetValue(chatId, out var state) && state is ProcessVideoDC)
-                    {
-                        await botClient.EditMessageText(
-                            statusMessage.Chat.Id,
-                            statusMessage.MessageId,
-                            Config.GetResourceString("DefaultActionTimeoutMessage"),
-                            cancellationToken: cancellationToken
-                        );
-
-                        _ = TelegramBot.HandleVideoRequest(botClient, link, chatId, statusMessage, targetUserIds, caption: text);
-                        
-                        TelegramBot.userStates.Remove(chatId, out _);
-                    }
-                }
-                catch (TaskCanceledException) {}
-            }, cancellationToken);
+            HandlersUtils.PrivateUtils.ProcessDefaultSendAction(botClient, chatId, statusMessage, defaultAction, cancellationToken,
+                                                                userId, defaultCondition, timeoutCTS, link, text);
         }
         else if (update.Message.Text == "/start")
         {
