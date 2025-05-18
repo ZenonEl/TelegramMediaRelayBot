2d7ff0c9bd2f45776cae18cf5c571cd53edaeeb4
ZenonEl <165126589+ZenonEl@users.noreply.github.com>
2025-05-18 17:05:15 +0400

    feat(MediaDownloader.cs): add temporary plug

--- TelegramBot/MediaDownloader.cs
+++ TelegramBot/MediaDownloader.cs
@@ -126,7 +126,7 @@ public partial class TGBot
                     }
                     else if (Config.showAccessDeniedMessage)
                     {
-                        await botClient.SendMessage(chatId, Config.GetResourceString("AccessDeniedMessage"), cancellationToken: cancellationToken, parseMode: ParseMode.Html);
+                        await botClient.SendMessage(chatId, string.Format(Config.GetResourceString("AccessDeniedMessage"), "https://mastodon.ml/@ZenonEl"), cancellationToken: cancellationToken, parseMode: ParseMode.Html);
                     }
                 }
 
